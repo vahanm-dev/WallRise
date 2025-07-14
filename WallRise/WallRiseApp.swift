8b584162c0b85280295428dfa8554c794e9bc0e8
vahanm-dev <dev.vahanm@gmail.com>
2025-07-14 15:18:48 +0400

    initial

---
+++ WallRise/WallRiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallRiseApp.swift
+//  WallRise
+//
+//  Created by Vahan Muradyan on 14.07.25.
+//
+
+import SwiftUI
+
+@main
+struct WallRiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
